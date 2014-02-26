b175f5292eb1c876e6b858a4e015f7920182bc5c
pauln <git@balboa.co.uk>
2014-02-26 12:37:45 +0000

    Change for code analysis

--- WindowMonitor/Program.cpp
+++ WindowMonitor/Program.cpp
@@ -12,7 +12,7 @@
 // WindowMonitor displays a DWM thumbnail for a selected window
 // TV Icon from http://www.iconarchive.com/show/media-icons-by-bevel-and-emboss/tv-icon.html
 
-int APIENTRY WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+int APIENTRY WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int)
 {
 	WindowFilter windowFilter;
 	PresetManager presetManager;
